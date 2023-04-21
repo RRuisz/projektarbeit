bf1f1bfb507c04e2a4a9636818b2881b6b0da242
rruisz <roman@ruisz.cc>
2023-04-21 13:38:37 +0200

    Creat Models / Controller & default layout

---
+++ app/Http/Controllers/NewspostController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewspostController extends Controller
+{
+    //
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lergola</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+<body class="bg-secondary">
+    <div class="container-fluid p-0 d-flex  bg-dark justify-content-between">
+        <nav class="navbar navbar-expand-lg p-0">
+            <div class="container-fluid bg-dark">
+              <a class="navbar-brand  text-white fs-4 ps-4 pe-4" href="{{ route('home')}}">Hotel Timeghost</a>
+              <button class="navbar-toggler bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <a class="nav-link text-info pe-5" href="{{route('news')}}">News</a>
+                  <a class="nav-link text-info pe-5" href="#">Übergaben</a>
+                  <a class="nav-link text-info pe-5" href="{{route('engineering')}}">Technik</a>
+                  {{-- <a class="nav-link text-info pe-5" href="#">Dienstplan</a> --}}
+                  <a class="nav-link text-info pe-5" href="#">Informationen</a>                  
+                  
+                </div>
+              </div>
+            </div>
+        </nav>
+        <div>
+          <p class="text-white pe-5 pt-1"> Angemeldet als: <a href="">Roman Ruisz{{-- $employee->name --}}</a> </p>
+        </div>
+      </div>
+    </div>
+    @yield('content')
+    @yield('scripts')
+</body>
+</html>
