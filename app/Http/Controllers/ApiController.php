29c96059ea4076df79bc299003a79953ca35f3be
rruisz <roman@ruisz.cc>
2023-05-11 19:29:12 +0200

    create users api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ApiController extends Controller
+{
+    public function users()
+    {
+        $user = User::all();
+        return $user;
+    }
+}
